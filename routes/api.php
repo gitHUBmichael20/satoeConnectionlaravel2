542f0f682d2baa43d6f6c46d4439a43b53267f8b
mufidibm <nmufidibrahim02@gmail.com>
2024-11-22 09:56:24 +0700

    Revert "Merge pull request #3 from gitHUBmichael20/pesan"
    This reverts commit 7032c293a037c03273ea7e14892b865c8a8449dd, reversing
    changes made to 02d6adcc34a6799dccfd67980cb4024f25e6fc05.

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,10 @@
 <?php
 
-use App\Http\Controllers\Auth\loginController;
-use App\Http\Controllers\Auth\registerController;
-use App\Http\Controllers\Auth\logoutController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\API\MessageController;
-
-
+use App\Http\Controllers\Auth\registerController;
+use App\Http\Controllers\Auth\loginController;
+use App\Http\Controllers\Auth\logoutController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -16,13 +13,3 @@ Route::get('/user', function (Request $request) {
 Route::post('/register', registerController::class);
 Route::post('/login', loginController::class);
 Route::post('/logout', logoutController::class)->middleware('auth:sanctum');
-
-Route::middleware('auth:sanctum')->group(function () {
-    // Chat routes
-    Route::prefix('messages')->group(function () {
-        Route::get('/conversations', [MessageController::class, 'getConversations']);
-        Route::get('/{userId}', [MessageController::class, 'getMessages']);
-        Route::post('/send', [MessageController::class, 'sendMessage']);
-        Route::put('/{messageId}/read', [MessageController::class, 'markAsRead']);
-    });
-});

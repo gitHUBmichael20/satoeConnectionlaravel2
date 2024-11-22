30422a964c8285f5d86769a3a99f916662a0376a
mufidibm <nmufidibrahim02@gmail.com>
2024-11-22 09:12:11 +0700

    rename AuthController.php to authController.php

--- app/Http/Controllers/API/authController.php
+++ app/Http/Controllers/API/authController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class AuthController extends Controller
+class authController extends Controller
 {
     //
 }
